6e35de8db1cda91ef65d813b972d54b56fa768a7
Finn <finn@finn.io>
2017-05-18 18:58:52 -0700

    Fix version

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 from distutils.core import setup
-from webhook_receiver import __version__
 
 setup(name='webhook-receiver',
-      version=__version__,
+      version="0.1.0",
       description='Recieves webhooks and executes commands.',
       author='Finn Herzfeld',
       author_email='finn@finn.io',
